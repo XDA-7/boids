4c0bf4ce492ff147a7b3d47c619915aff3e2a840
XDA-7 <xaraxsaren@hotmail.com.au>
2017-07-23 02:06:47 +1000

    Change to triangles, need steering and more sophisticated detection

--- src/boid.py
+++ src/boid.py
@@ -5,66 +5,94 @@ The three critical rules governing boid behaviour are
 2. Alignment: Set velocity to move in the average direction of local boids
 3. Cohesion: Set velocity to move towards the centre of mass of local boids
 """
-import time
-import tkinter
+from typing import List
+from vector import Vector
 
-class Window:
-    """The main window in which the process will be hosted"""
-    def __init__(self):
-        self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, bg='grey', height=1000, width=1000)
-        self.canvas.pack()
-        self.last_time = time.monotonic()
-        self.boids = []
-
-    def update(self):
-        """Event loop body"""
-        delta_time = time.monotonic() - self.last_time
-        for boid in self.boids:
-            delta_x = boid.x_velocity * delta_time
-            delta_y = boid.y_velocity * delta_time
-            self.canvas.move(boid.canvas_handle, delta_x, delta_y)
-        self.last_time = time.monotonic()
-        self.window.update()
-
-    def add_boid(self, boid):
-        """Add a boid to the canvas"""
-        boid.canvas_handle = self.canvas.create_oval(boid.bounding_box(), fill='blue')
-        self.boids.append(boid)
+TRIANGLE_POINTS = (Vector(0, 5), Vector(5, -5), Vector(-5, -5))
 
 class Boid:
     """Simple Automaton"""
-    def __init__(self, radius, vision_radius, x_position, y_position, x_velocity, y_velocity):
+    separation_velocity_multiplier = 0.1
+    alignment_velocity_multiplier = 0.1
+    cohesion_velocity_multiplier = 0.1
+
+    def __init__(self, radius: float, vision_radius: float,
+                 x_position: float, y_position: float, rotation: float):
         self.radius = radius
         self.vision_radius = vision_radius
         self.vision_radius_squared = vision_radius ** 2
         self.x_position = x_position
         self.y_position = y_position
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
+        self.rotation = rotation
+        self.x_velocity = 0
+        self.y_velocity = 0
 
-    def bounding_box(self):
-        """Returns the x,y coords of the top-left and bottom right corners of the bounding box"""
+    def bounding_box(self) -> tuple:
+        """OBSOLETE: The boid is now represented by a triangle
+        Returns the x,y coords of the top-left and bottom right corners of the bounding box"""
         upper_left_x = self.x_position - self.radius
         upper_left_y = self.y_position + self.radius
         bottom_right_x = self.x_position + self.radius
         bottom_right_y = self.y_position - self.radius
         return (upper_left_x, upper_left_y, bottom_right_x, bottom_right_y)
 
-    def update(self, boids):
+    def points(self) -> tuple:
+        points = []
+        for point in TRIANGLE_POINTS:
+            rotated_point = point.rotate(self.rotation)
+            points.append(rotated_point.x_val + self.x_position)
+            points.append(rotated_point.y_val + self.y_position)
+        return tuple(points)
+
+    def update(self, boids: List['Boid']):
         """Adjusts the boid's velocity depending on neighbouring boids"""
         local_boids = self.find_neighbouring_boids(boids)
-        print(local_boids)
+        print('local boids count: ' + str(local_boids.__len__()))
+        separation = self.separation_velocity(local_boids) * Boid.separation_velocity_multiplier
+        alignment = self.alignment_velocity(local_boids) * Boid.alignment_velocity_multiplier
+        cohesion = self.cohesion_velocity(local_boids) * Boid.cohesion_velocity_multiplier
+        self.x_velocity = separation.x_val + alignment.x_val + cohesion.x_val
+        self.y_velocity = separation.y_val + alignment.y_val + cohesion.y_val
 
-    def separation_velocity(self, local_boids):
+    def velocity(self) -> Vector:
+        return Vector(self.x_velocity, self.y_velocity)
+
+    def separation_velocity(self, local_boids: List['Boid']) -> Vector:
         """Calculates the velocity to avoid local boids"""
-        pass
+        locals_to_self_x = 0
+        locals_to_self_y = 0
+        for boid in local_boids:
+            locals_to_self_x += self.x_position - boid.x_position
+            locals_to_self_y += self.y_position - boid.y_position
+        return Vector(locals_to_self_x, locals_to_self_y).normalized()
 
+    def alignment_velocity(self, local_boids: List['Boid']) -> Vector:
+        """Calculates the average velocity of all local boids"""
+        alignment_x = 0
+        alignment_y = 0
+        for boid in local_boids:
+            alignment_x += boid.x_velocity
+            alignment_y += boid.y_velocity
+        return Vector(alignment_x, alignment_y).normalized()
 
-    def find_neighbouring_boids(self, boids):
+    def cohesion_velocity(self, local_boids: List['Boid']) -> Vector:
+        """Calculates the direction towards the center of mass of all local boids"""
+        position_x = 0
+        position_y = 0
+        boid_count = local_boids.__len__()
+        for boid in local_boids:
+            position_x += boid.x_position
+            position_y += boid.y_position
+        position_x /= boid_count
+        position_y /= boid_count
+        return Vector(position_x - self.x_position, position_y - self.y_position).normalized()
+
+    def find_neighbouring_boids(self, boids: List['Boid']) -> List['Boid']:
         """Find all boids within the self's range of vision"""
         local_boids = []
         for boid in boids:
+            if(boid == self):
+                continue
             distance_squared = (self.x_position - boid.x_position) ** 2
             distance_squared += (self.y_position - boid.y_position) ** 2
             if distance_squared <= self.vision_radius_squared:
--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
-import boid
+"""Program entry point"""
 
-window = boid.Window()
-flock = [
-boid.Boid(5, 30, 500, 500, 0, 0),
-boid.Boid(5, 30, 520, 500, 0, 0),
-boid.Boid(5, 30, 480, 500, 0, 0),
-boid.Boid(5, 30, 500, 520, 0, 0),
-boid.Boid(5, 30, 500, 480, 0, 0)
+import typing
+import boid
+import window
+
+FLOCK = typing.List[boid.Boid]
+ITERATIONS = 0
+
+window = window.Window()
+flock: FLOCK = [
+    boid.Boid(5, 100, 500, 500, 0),
+    boid.Boid(5, 100, 550, 500, 1.5),
+    boid.Boid(5, 100, 480, 500, -1.5),
+    boid.Boid(5, 100, 500, 520, 3.14),
+    boid.Boid(5, 100, 500, 430, 3.14)
 ]
 
 for boid in flock:
     window.add_boid(boid)
 
-for boid in flock:
-    boid.update(flock)
-
 while True:
-    # for boid in flock:
-    #     boid.update(flock)
+    for boid in flock:
+        boid.update(flock)
     window.update()
+    ITERATIONS += 1
+    print(ITERATIONS)
